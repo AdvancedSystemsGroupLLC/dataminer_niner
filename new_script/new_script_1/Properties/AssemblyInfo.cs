b90a861c782e6105d1fa6687d9e4ba37602fb565
Tim Scott <tim@asgllc.com>
2023-05-16 15:41:27 -0400

    Init

---
+++ new_script/new_script_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("new_script_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("new_script_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("80C2C410-BB55-44EA-9112-41A9E305478A")]
+[assembly: AssemblyVersion("1.0.0.0")]
